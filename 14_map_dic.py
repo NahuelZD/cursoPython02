e7512551e22b945b176fc73babd8feb192d3c3ad
NahuelZD <nahuelmassa93@gmail.com>
2023-07-07 20:15:32 -0300

    map al diccionario sin modificar el original

--- 14_map_dic.py
+++ 14_map_dic.py
@@ -17,3 +17,21 @@ items = [
 
 prices = list(map(lambda item : item['price'], items))
 print(prices)
+
+# Agregar un atributo al dicc
+def add_taxes(item):
+    item['taxes'] = item['price'] * .21
+    return item
+
+new_items = list(map(add_taxes, items))
+print(items)
+
+# Esto anterior me modifica el array original
+# Para eso, tenemos que crear una copia del diccionario anterio
+
+'''
+def add_taxes(item):
+    new_item = item.copy.()
+    new_item['taxes'] = new_item['price'] * .21
+    return new_item
+'''
