be7150cc40e73174ff6dad97afad4f55d35b127f
NahuelZD <nahuelmassa93@gmail.com>
2023-07-09 11:53:34 -0300

    __init__

---
+++ pkg/__init__.py
@@ -0,0 +1,8 @@
+# De la versión 3.3 para atrás era obligatorio. Ahora es para inicializar alguna variable
+print('Se inició paquete')
+
+URL = 'Platzi.com'
+
+import pkg.mod_1, pkg.mod_2
+
+# Cada vez que aprieto run llamo a este paquete
