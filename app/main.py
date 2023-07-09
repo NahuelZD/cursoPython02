67371c1f5d9f93e3ce152c9db59b44db9817d107
NahuelZD <nahuelmassa93@gmail.com>
2023-07-09 11:33:59 -0300

    Entry Point __main__

--- app/main.py
+++ app/main.py
@@ -1,11 +1,5 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys, values)
-
-text = utils.A
-print(text)
-
 data = [
     {
         'Country': 'Colombia',
@@ -17,5 +11,31 @@ data = [
     }
 ]
 
-result = utils.population_by_country(data, 'Bolivia')
-print(result)
+def run():
+    keys, values = utils.get_population()
+    print(keys, values)
+
+    text = utils.A
+    print(text)
+    
+    result = utils.population_by_country(data, 'Bolivia')
+    print(result)
+    
+if __name__ == '__main__':
+    run() # Se le llama: Entry point
+
+'''
+MÓDULOS COMO SCRIPTS: NAME y MAIN
+
+Cuando utilizamos name == ‘main’ estamos dando dualidad a cierta función para que sea ejecutada en dos archivos distintos.
+
+Para ello debemos tener en cuenta que su uso esta catalogado de dos maneras:
+
+Se puede ejecutar el archivo como un script.
+Importando el codding de un archivo a otro archivo python.
+Para Python, es independiente cual de las dos formas estemos utilizando el código, ya que python define una variable especial llamada __name__ la cual contiene un string y cuyo resultado dependerá de la forma en como sea usada.
+
+Como en el ejemplo, se observa que el primer archivo que denominamos main.py.
+
+main.py
+'''
