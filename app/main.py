2bb464d068f1a6a9ab480094db1561740a30c2be
NahuelZD <nahuelmassa93@gmail.com>
2023-07-19 10:50:34 -0300

    Lectura de csv y proyecto

--- app/main.py
+++ app/main.py
@@ -1,25 +1,17 @@
 import utils
-
-data = [
-    {
-        'Country': 'Colombia',
-        'Population': 300
-    },
-    {
-        'Country': 'Bolivia',
-        'Population': 500
-    }
-]
+import read_csv
+import charts
 
 def run():
-    keys, values = utils.get_population()
-    print(keys, values)
-
-    text = utils.A
-    print(text)
     
-    result = utils.population_by_country(data, 'Bolivia')
-    print(result)
+    data = read_csv.read_csv('./app/data.csv')
+    country = input('Type country -> ')
+    result = utils.population_by_country(data, country)
+    
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
     
 if __name__ == '__main__':
     run() # Se le llama: Entry point
